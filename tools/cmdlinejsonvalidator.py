0b66b2ad1c884625149d4941df773186d04491aa
Allen D. Householder <adh@cert.org>
2017-04-05 11:48:40 -0400

    clean up spacing

--- tools/cmdlinejsonvalidator.py
+++ tools/cmdlinejsonvalidator.py
@@ -31,38 +31,35 @@ from jsonschema import validate
 from jsonschema import Draft4Validator
 
 
-
 def jsonvalidation(json_doc_path, json_schema_path):
+    with open(json_schema_path, 'r') as fp:
+        schema_doc = json.load(fp)
 
-	with open(json_schema_path, 'r') as fp:
-		schema_doc=json.load(fp)
+    # Open the file for reading
+    with open(json_doc_path, 'r') as fp:
+        json_doc = json.load(fp)
 
-	# Open the file for reading
-	with open(json_doc_path, 'r') as fp:
-		json_doc = json.load(fp)
+    try:
+        validate(json_doc, schema_doc)
+        sys.stdout.write("Record passed validation \n")
+    except jsonschema.exceptions.ValidationError as incorrect:
+        v = Draft4Validator(schema_doc)
+        errors = sorted(v.iter_errors(json_doc), key=lambda e: e.path)
+        for error in errors:
+            sys.stderr.write("Record did not pass: \n")
+            sys.stderr.write(str(error.message) + "\n")
 
-	try:
-		validate(json_doc,schema_doc)
-		sys.stdout.write("Record passed validation \n")
-	except jsonschema.exceptions.ValidationError as incorrect:
-		v = Draft4Validator(schema_doc)
-		errors = sorted(v.iter_errors(json_doc), key=lambda e: e.path)
-		for error in errors:
-			sys.stderr.write("Record did not pass: \n")
-			sys.stderr.write(str(error.message)+ "\n")
 
 def main():
-	import argparse
+    import argparse
 
-	parser = argparse.ArgumentParser(description='validate a JSON file')
-	parser.add_argument('jsondoc', type=str, help='path/to/doc.json')
-	parser.add_argument('schema', type=str, help='path/to/schema.json')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='validate a JSON file')
+    parser.add_argument('jsondoc', type=str, help='path/to/doc.json')
+    parser.add_argument('schema', type=str, help='path/to/schema.json')
+    args = parser.parse_args()
 
-	jsonvalidation(args.jsondoc, args.schema)
+    jsonvalidation(args.jsondoc, args.schema)
 
 
-
-if __name__=='__main__':
-	main()
-
+if __name__ == '__main__':
+    main()
